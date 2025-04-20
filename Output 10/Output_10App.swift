eca8f443272735e42669123b589dbc4770d8c24c
Christa Kulanda <christakulanda@gmail.com>
2025-04-19 21:49:01 -0500

    Initial Commit

---
+++ Output 10/Output_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Output_10App.swift
+//  Output 10
+//
+//  Created by Scholar on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Output_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Output 10Tests/Output_10Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Output_10Tests.swift
+//  Output 10Tests
+//
+//  Created by Scholar on 4/19/25.
+//
+
+import Testing
+@testable import Output_10
+
+struct Output_10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
